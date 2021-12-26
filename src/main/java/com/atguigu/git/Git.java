c80c3004100eed52909a590685f9a2153b759f36
liangrui <2811196219@qq.com>
2021-12-26 16:22:39 +0800

    master test commit

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -5,5 +5,6 @@ public class Git {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello master-test");
     }
 }
