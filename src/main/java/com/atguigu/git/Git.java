28e592b511ba691436cf964df1e008f4298483e0
liangrui <2811196219@qq.com>
2021-12-26 16:21:30 +0800

    hot-fix second commit

--- src/main/java/com/atguigu/git/Git.java
+++ src/main/java/com/atguigu/git/Git.java
@@ -5,6 +5,6 @@ public class Git {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
-        System.out.println("hello git3");
+        System.out.println("hello hot-fix-test");
     }
 }
